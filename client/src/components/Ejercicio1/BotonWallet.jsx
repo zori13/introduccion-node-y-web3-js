22675f862889755238f039800505577756e9b5f6
zori13 <zori.gon@mail.com>
2023-12-06 14:56:53 +0100

    Desconectar billetera

--- client/src/components/Ejercicio1/BotonWallet.jsx
+++ client/src/components/Ejercicio1/BotonWallet.jsx
@@ -1,6 +1,8 @@
 import Web3 from "web3"
-
+import {useState}from'react'
 export default function BotonWallet(){
+    const [cuenta,setCuenta]=useState(null)
+    const [balance,setBalance]=useState(null)
     const conectWallet = async()=>{
         try{
             if(window.ethereum){
@@ -9,10 +11,16 @@ export default function BotonWallet(){
                 await window.ethereum.enable()
                 const cuentas= await web3Instance.eth.getAccounts()
                 if (cuentas.length>0){
+                    setCuenta(cuentas[0])
                     console.log('Cuentas: ', cuentas)
+                   
                 }else{
                     throw new Error('No hay cuenta en esta billetera')
                 }
+                const balance =await web3Instance.eth.getBalance(cuentas[0])
+                const formattedBalance=web3Instance.utils.fromWei(balance, 'ether')
+                console.log('Balance :', balance)
+                setBalance(formattedBalance)
             }else{
                 throw new Error('No se encuentra billetera Ethereum en el ordenador')
             }
@@ -20,5 +28,24 @@ export default function BotonWallet(){
             console.log('Error al conectar la billetera:', error)
         }
        }
-    return  <button onClick={conectWallet}>Conectar</button>        
-}
+
+       const desconectarWallet=()=>{
+        setCuenta(null)
+        setBalance(null)
+       }
+      
+    return (
+    <div>
+        {cuenta ?(
+            <>
+                <p>Concetado a la billetera : {cuenta} </p>
+                <p>Balance : {balance} </p>
+                <button onClick={desconectarWallet}>Desconectar</button>
+            </>
+        ):(
+            <button onClick={conectWallet}>Conectar</button>         
+        )} 
+        
+    </div>
+        )
+    }
